ea08c08112ec52391d6c9bc182f9b58b58eccae3
illusir <illusive.walker@gmail.com>
2019-03-17 17:25:59 +0400

    1703 js test

---
+++ js/test-script.js
@@ -0,0 +1,45 @@
+//number
+
+var a = 10;
+alert(a + " has data type: " + typeof(a));
+
+console.log(typeof(a));
+
+//string
+
+var b = "Text Sample";
+alert(b + " has data type: " + typeof(b));
+
+console.log(typeof(b));
+
+//boolean
+
+var c = true;
+alert(c + " has data type: " + typeof(c));
+
+console.log(typeof(c));
+
+//object
+
+var d = {
+    a: 1,
+    b: 2,
+    c: 3
+};
+alert(d + " has data type: " + typeof(d));
+
+console.log(typeof(d));
+
+//null
+
+var e = null;
+alert(e + " has data type: " + typeof(e) + " (no null type yet)");
+
+console.log(typeof(e));
+
+//undefined
+
+var f;
+alert(f + " has data type: " + typeof(f));
+
+console.log(typeof(f));
