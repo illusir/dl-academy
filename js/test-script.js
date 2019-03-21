4519525e35c180834345196022b07044c6737409
illusir <illusive.walker@gmail.com>
2019-03-21 19:35:43 +0400

    2103 js

--- js/test-script.js
+++ js/test-script.js
@@ -1,45 +1,25 @@
-//number
+//Let's Play
 
-var a = 10;
-alert(a + " has data type: " + typeof(a));
+var min = 1000;
+var max = 9999;
+var num = rndmnum(min, max);
+var urnum = prompt('Your number? Enter 4 digits!');
 
-console.log(typeof(a));
+function rndmnum(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
-//string
+if (isNaN(urnum)) {
+	alert('Your answer: ' + urnum + ' is not a number!');
+} else if (urnum === 'undefined' || urnum === '') {
+   alert('Nope, pick a number!');
+   prompt();
+   } else if (urnum > num) {
+     alert('Your answer: ' + urnum + ' is bigger than required!');
+   } else if (urnum < num) {
+     alert('Your answer: ' + urnum + ' is smaller than required!');
+   } else {
+       alert('Your answer: ' + urnum + ' is correct!');
+       }
 
-var b = "Text Sample";
-alert(b + " has data type: " + typeof(b));
-
-console.log(typeof(b));
-
-//boolean
-
-var c = true;
-alert(c + " has data type: " + typeof(c));
-
-console.log(typeof(c));
-
-//object
-
-var d = {
-    a: 1,
-    b: 2,
-    c: 3
-};
-alert(d + " has data type: " + typeof(d));
-
-console.log(typeof(d));
-
-//null
-
-var e = null;
-alert(e + " has data type: " + typeof(e) + " (no null type yet)");
-
-console.log(typeof(e));
-
-//undefined
-
-var f;
-alert(f + " has data type: " + typeof(f));
-
-console.log(typeof(f));
+alert('And the answer is ' + num + '!');
