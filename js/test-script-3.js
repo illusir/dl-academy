b3fd7f60da7188a1a730941b8b465dc4da5e0a72
illusir <illusive.walker@gmail.com>
2019-03-28 21:41:21 +0400

    280319 Work in progress

---
+++ js/test-script-3.js
@@ -0,0 +1,26 @@
+
+function numToObj(num) {
+  num = prompt('Enter the number from 0 to 999');
+  var  numContains = {
+    "Units": 0,
+    "Dozens": 0,
+    "Hundreds": 0
+  }
+while (num != 999) {
+  if (num > 999) {
+    num = console.log('Number is bigger than required');
+    numContains = {};
+    num = prompt('Enter the number from 0 to 999');
+} else if (num<=999){
+  numContains["Units"] = Math.floor(num % 10);
+  numContains["Dozens"] = Math.floor(num/10 % 10);
+  numContains["Hundreds"] = Math.floor(num /100 % 10);
+
+  console.log('==========');
+  console.log('Number ' + num + ' parts counter:');
+  console.log(numContains);
+  num = prompt('Enter the number from 0 to 999');
+}
+}
+}
+numToObj();
