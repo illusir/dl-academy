cf0944f1d1f709eecce328ee6d4cf2ee734001b7
illusir <illusive.walker@gmail.com>
2019-04-03 00:44:00 +0400

    0304 JS Assignments

--- JS-assignments/JS-assignment-5/script-assignment-5.js
+++ JS-assignments/JS-assignment-5/script-assignment-5.js
@@ -1,1 +1,24 @@
 "use strict";
+
+window.onload = function(){
+
+  function chess() {
+
+    var chessField = document.getElementById('checker');
+    var hor = 8;
+    var ver = 8;
+
+    for (var i = 0; i < ver; i++) {
+      var row = chessField.appendChild(document.createElement('div'));
+      row.classList.add('line');
+
+      for (var j = 0; j < hor; j++) {
+        row.appendChild(document.createElement('span'));
+      }
+    }
+
+  }
+
+chess();
+
+}
