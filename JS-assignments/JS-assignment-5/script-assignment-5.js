634c0b97267bb92d36c4f8ec989cc518c87092f5
illusir <illusive.walker@gmail.com>
2019-04-05 01:49:02 +0400

    0504 chess

--- JS-assignments/JS-assignment-5/script-assignment-5.js
+++ JS-assignments/JS-assignment-5/script-assignment-5.js
@@ -4,17 +4,60 @@ window.onload = function(){
 
   function chess() {
 
+    //variables
+
     var chessField = document.getElementById('checker');
     var hor = 8;
     var ver = 8;
+    var numSpace = ver - 1;
+
+    //numbers and letters
+
+    var num = [1, 2, 3, 4, 5, 6, 7, 8];
+    var letter = [' ','A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', ' '];
+
+    //figures
+
+    var figure = {
+        k: {
+        	name: 'King'
+        },
+        q: {
+          name: 'Queen'
+        },
+        r: {
+        	name: 'Rook'
+        },
+        e: {
+        	name: 'Elephant'
+        },
+        h: {
+        	name: 'Horse'
+        },
+        p: {
+        	name: 'Pawn'
+        }
+      }
+
+    //main board
 
     for (var i = 0; i < ver; i++) {
       var row = chessField.appendChild(document.createElement('div'));
       row.classList.add('line');
-
-      for (var j = 0; j < hor; j++) {
-        row.appendChild(document.createElement('span'));
+      if (i >= 0 && i <= numSpace) {
+      row.innerHTML = num[i];
       }
+          for (var j = 0; j < hor; j++) {
+            var span = row.appendChild(document.createElement('span'));
+            if (i == numSpace) {
+            var letField = chessField.appendChild(document.createElement('div'));
+            var singleLet = letField.appendChild(document.createElement('div'));
+            singleLet.classList.add('letter');
+            singleLet.innerHTML = letter[j];
+          }
+
+          }
+
     }
 
   }
